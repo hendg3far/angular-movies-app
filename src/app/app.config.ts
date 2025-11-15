1fd814d981cea32b4d1046450d23d7cd8b80d230
Hend Ali Gafar <hend@192.168.1.96>
2025-11-15 07:39:21 +0200

    Add navbar design

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,12 +4,14 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideHttpClient } from '@angular/common/http';
 
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideHttpClient(),
     importProvidersFrom(BrowserAnimationsModule, NgxSpinnerModule)
   ]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './features/home/home.component';
+import { MoviesComponent } from './features/movies/movies.component';
+import { TvComponent } from './features/tv/tv.component';
+import { SearchComponent } from './core/components/search/search.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent
+    }, {
+        path: 'movie',
+        component: MoviesComponent
+    }, {
+        path: 'tv',
+        component: TvComponent
+    }, {
+        path: 'search',
+        component: SearchComponent
+    }, {
+        path: '**',
+        redirectTo: '',
+    },
+];
