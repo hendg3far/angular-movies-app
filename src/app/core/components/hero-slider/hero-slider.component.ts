d137224b8312c913ec10aba6c51a7427e49b7485
portal2 <portal2@server.tanta.edu.eg>
2025-11-16 10:53:02 +0200

    add hero slide func

--- src/app/core/components/hero-slider/hero-slider.component.ts
+++ src/app/core/components/hero-slider/hero-slider.component.ts
@@ -1,17 +1,20 @@
-import { Component } from '@angular/core';
-import { SwiperComponent } from "swiper/angular";
-// import Swiper core and required modules
-import SwiperCore, { Swiper, Virtual } from 'swiper';
-SwiperCore.use([Virtual]);
+import { CommonModule } from '@angular/common';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, Input } from '@angular/core';
+import { TruncatePipe } from '../../pipes/elipsis.pipe';
+import { NumberWithCommasPipe } from '../../pipes/number-with-commas.pipe';
+import { RouterLink } from '@angular/router';
+import { register } from 'swiper/element/bundle';
 
+register();
 
 @Component({
   selector: 'app-hero-slider',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, CommonModule, TruncatePipe, NumberWithCommasPipe],
   templateUrl: './hero-slider.component.html',
-  styleUrls: ['./hero-slider.component.scss']
+  styleUrls: ['./hero-slider.component.scss'],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class HeroSliderComponent {
-
+  @Input() slides: any[] = [];
 }
