77e0d3642eb5e6f8169165228c92045e9b97fed6
Hend Ali Gafar <hend@192.168.1.107>
2025-11-16 07:25:01 +0200

    add swiper html

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,6 +7,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { provideHttpClient } from '@angular/common/http';
 
 
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
--- src/app/core/components/hero-slider/hero-slider.component.ts
+++ src/app/core/components/hero-slider/hero-slider.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { SwiperComponent } from "swiper/angular";
+// import Swiper core and required modules
+import SwiperCore, { Swiper, Virtual } from 'swiper';
+SwiperCore.use([Virtual]);
+
 
 @Component({
   selector: 'app-hero-slider',
   standalone: true,
   imports: [],
   templateUrl: './hero-slider.component.html',
-  styleUrl: './hero-slider.component.scss'
+  styleUrls: ['./hero-slider.component.scss']
 })
 export class HeroSliderComponent {
 
